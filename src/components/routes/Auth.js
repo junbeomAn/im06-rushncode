d628219cf89655953cf89f989ba0fe01127e7dff
Junbeom_An <wnsqja8744@gmail.com>
2018-05-10 21:12:55 +0900

    tag, rank login, signup, home

--- src/components/routes/Auth.js
+++ src/components/routes/Auth.js
@@ -1,11 +1,52 @@
 import React from 'react';
+import { Input, Button, Form, Divider, Icon, Segment } from 'semantic-ui-react';
+import 'react-flexview/lib/flexView.css';
+// import FlexView from 'react-flexview';
+import '../../styles/css/Auth.css';
 
-const Auth = () => {
-  return (
-    <div>
-      Auth 페이지 입니다.
+const Auth = () => (
+  <div className="authContainer">
+    <div className="authInputContainer">
+      <div className="authButtonContainer">
+        <Button fluid color="google plus" className="connectLogin">
+          <Icon name="google plus" /> Google Plus
+        </Button>
+      </div>
+      <div className="authButtonContainer">
+        <Button fluid color="facebook" className="connectLogin">
+          <Icon name="facebook" /> Facebook
+        </Button>
+      </div>
+      <Divider horizontal className="authDivider">
+        OR
+      </Divider>
+      <div className="authFormContainer">
+        <Form action="http://localhost:3001/auth/signin" method="post">
+          <Form.Field>
+            <Input icon="at" name="email" placeholder="E-mail address" size="medium" />
+          </Form.Field>
+          <Form.Field>
+            <Input
+              icon="lock"
+              name="password"
+              type="password"
+              placeholder="password"
+              size="medium"
+            />
+          </Form.Field>
+          <Button type="submit">Sign In dude</Button>
+        </Form>
+      </div>
+      <Segment textAlign="center">
+        <p>
+          Did you forget your password? <a href="/">Find</a>
+        </p>
+        <p>
+          Don't you have an account? <a href="/">Sign Up</a>
+        </p>
+      </Segment>
     </div>
-  );
-};
+  </div>
+);
 
 export default Auth;
--- src/components/routes/Tag.js
+++ src/components/routes/Tag.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Input } from 'semantic-ui-react';
+import '../../styles/css/Tag.css';
 
 const Tag = () => {
   return (
-    <div>
-      Tag 페이지 입니다.
+    <div className="tagContainer">
+      <div className="tagInputContainer">
+        <Input placeholder="search tags here. . ." />
+      </div>
+      
     </div>
   );
 };
