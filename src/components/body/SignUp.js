19d71ccb914512404adce2a286cf115d04df1f71
Junbeom_An <wnsqja8744@gmail.com>
2018-05-25 18:34:18 +0900

    commit before merge

--- src/components/body/SignUp.js
+++ src/components/body/SignUp.js
@@ -8,12 +8,12 @@ const SignUp = ({ keyPress, signUp }) => (
       <div className="authButtonContainer">
         <a href={`https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&access_type=offline&include_granted_scopes=true&state=state_parameter_passthrough_value&redirect_uri=http://localhost:3000/auth/google&response_type=code&client_id=1057319403388-ng0dluqb41b5kk4gt37bot92piirjiu6.apps.googleusercontent.com`}>
           <Button color="google plus" className="authBtn">
-            <Icon name="google" />Google
+            <Icon name="google" />Login with Google
           </Button>
         </a>
         <a href="https://github.com/login/oauth/authorize?client_id=9eccd23df65b6d3581f9">
           <Button color="black" className="authBtn">
-            <Icon name="github" />Github
+            <Icon name="github" />Login with Github
           </Button>
         </a>
       </div>
@@ -68,17 +68,19 @@ const SignUp = ({ keyPress, signUp }) => (
             />
           </div>
         </div>
-        <button type="button" className="btn btn-secondary authBtn">
-              로그인
-        </button>
-        <button
-          onClick={() => {
-            signUp();
-          }}
-          className="btn btn-primary authBtn"
-        >
-          계정생성
-        </button>
+        <div className="authButtonContainer">
+          <button type="button" className="btn btn-secondary authBtn">
+                로그인
+          </button>
+          <button
+            onClick={() => {
+              signUp();
+            }}
+            className="btn btn-primary authBtn"
+          >
+            계정생성
+          </button>
+        </div>
       </div>
     </div>
   </div>
