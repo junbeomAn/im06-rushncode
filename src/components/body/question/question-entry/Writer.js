e6e9c16efdc00cfd2eed089f948f3ed74d40024e
hanyoungjae <han6318@naver.com>
2018-05-26 15:22:53 +0900

    mypage profile image upload complete

--- src/components/body/question/question-entry/Writer.js
+++ src/components/body/question/question-entry/Writer.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import image from '../../../../images/profile/coding.png';
 
-const Writer = ({ username, userID }) => (
+const Writer = ({
+  username, userID, image, aImage,
+}) => (
   <a className="ItemBox likeBtn" href={`/mypage/${userID}`}>
     <div className="ItemBoxNum">
-      <img src={image} alt="사용자" width={40} style={{ borderRadius: '5px' }} />
+      {aImage ? (
+        <img
+          src={require(`../../../../images/profile/${aImage}_mini.png`)}
+          style={{ borderRadius: '5px' }}
+          alt="image"
+        />
+      ) : (
+        <img
+          src={require(`../../../../images/profile/${image}_mini.png`)}
+          style={{ borderRadius: '5px' }}
+          alt="image"
+        />
+      )}
     </div>
     <div className="ItemBoxImage">{username}</div>
   </a>
