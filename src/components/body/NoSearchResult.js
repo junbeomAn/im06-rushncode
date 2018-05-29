48a5cea9949fda3ef737daa2c2462e8898eed2d7
Junbeom_An <wnsqja8744@gmail.com>
2018-05-29 20:15:19 +0900

    search no result page ongoing

--- src/components/body/NoSearchResult.js
+++ src/components/body/NoSearchResult.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const NoSearchResult = () => (
   <div className="no-result-container">
-    <h1 className="no-result">
-      <span className="no-result-keyword">'{ decodeURI(window.location.href).split('?q=')[1]}'</span>&nbsp;에 대한 검색 결과가 없습니다.
+    <h1 className="no-result-title">러시앤코드</h1><br /><br /><br />
+    <h1 className="no-result-text">
+      <span className="no-result-keyword">&rsquo;{ decodeURI(window.location.href).split('?q=')[1]}&rsquo;</span>&nbsp;에 대한 검색 결과가 없습니다.
     </h1>
   </div>
 );
